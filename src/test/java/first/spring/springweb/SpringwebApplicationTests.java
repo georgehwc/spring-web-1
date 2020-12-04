0938f7c3b0a6c218558feb326467a7442679a859
georgechw <georgechan223@gmail.com>
2020-12-04 22:39:29 +0800

    first

---
+++ src/test/java/first/spring/springweb/SpringwebApplicationTests.java
@@ -0,0 +1,12 @@
+package first.spring.springweb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringwebApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+}
